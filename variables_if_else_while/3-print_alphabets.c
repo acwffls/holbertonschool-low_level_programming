72dc16d3a9d4d3c6d9edd750f46907ff0d4e9999
acwffls <wflsac@gmail.com>
2024-02-21 12:01:34 +0100

    3-print_alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low, high;
+
+	for (low = 'a'; low <= 'z'; low++)
+{
+	putchar(low);
+}
+
+	for (high = 'A'; high <= 'Z'; high++)
+{
+	putchar(high);
+}
+	putchar('\n');
+	return (0);
+}
