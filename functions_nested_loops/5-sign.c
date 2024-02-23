9533e11b0f6c18f63274131b2fd8c510b06c3329
acwffls <wflsac@gmail.com>
2024-02-23 16:27:38 +0100

    5-sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * print_sign - look the description
@@ -10,19 +11,24 @@
 
 int print_sign(int n)
 {
-if (n > 0)
+	char sign;
+
+	if (n > 0)
 {
-	printf("+");
+	sign = '+';
+	write(1, &sign, 1);
 	return (1);
 }
-else if (n == 0)
+	else if (n == 0)
 {
-printf("0");
+	sign = '0';
+	write(1, &sign, 1);
 	return (0);
 }
-else
+	else
 {
-printf("-");
+	sign = '-';
+	write(1, &sign, 1);
 	return (-1);
 }
 }
