21e8ec22b34d933320af9b906e66a84aa06dd293
acwffls <wflsac@gmail.com>
2024-02-28 10:46:25 +0100

    4-print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - look description
+ * description: write a function that prints a string in reverse
+ * @s: an input string
+ * return: nothing
+*/
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+
+		len++;
+
+	while (len)
+
+		_putchar(s[--len]);
+
+	_putchar('\n');
+}
