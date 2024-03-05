80142674512980907f69dc059d1a1ed361a81f70
acwffls <wflsac@gmail.com>
2024-03-05 09:54:05 +0100

    1-memcpy.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,4 +20,5 @@ void reverse_array(int *a, int n);
 char *cap_string(char *);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
