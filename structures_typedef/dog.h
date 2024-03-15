b41a63d274fdd70d9b6d244d77a75158b11c685a
acwffls <wflsac@gmail.com>
2024-03-15 20:22:32 +0100

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's identity
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
